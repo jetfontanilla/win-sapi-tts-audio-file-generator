a372929270378d5b5fddba57a730cd4a9b74735f
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2020-06-17 16:56:29 +0300

    add support for American English Phoneme Representation

--- generate.py
+++ generate.py
@@ -14,6 +14,9 @@ def _read_transcript_lines():
     req_file_path = '%s/transcripts.csv' % dirname(realpath(__file__))
     return open(req_file_path, 'r')
 
+def _generate_phoneme_xml(phonemes):
+    return '<PRON SYM="{phonemes}"/>'.format(phonemes=phonemes.replace('&', '&amp;'))
+
 
 config = _load_config()
 voice = tts.sapi.Sapi()
@@ -23,5 +26,12 @@ voice.set_rate(int(config['rate']))
 voice.set_volume(int(config['volume']))
 
 reader = csv.reader(_read_transcript_lines())
+
+flag_xml = 8
+flag_not_xml = 16
+
 for row in reader:
-    voice.create_recording('output/' + row[0] + '.wav', row[1])
+    cols = len(row)
+    sapi_input = _generate_phoneme_xml(row[2]) if cols > 2 else row[1]
+    sapi_flags = flag_xml if cols > 2 else flag_not_xml
+    voice.create_recording('output/' + row[0] + '.wav', sapi_input, sapi_flags)
