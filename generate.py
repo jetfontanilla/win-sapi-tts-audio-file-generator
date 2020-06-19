5f1df40e637ad2776a93dbee1271b849dac0b978
Jet Fontanilla <jet.fontanilla@englishcentral.com>
2020-06-19 13:41:40 +0300

    use SSML for phoneme-based TTS

--- generate.py
+++ generate.py
@@ -14,8 +14,23 @@ def _read_transcript_lines():
     req_file_path = '%s/transcripts.csv' % dirname(realpath(__file__))
     return open(req_file_path, 'r')
 
-def _generate_phoneme_xml(phonemes):
-    return '<PRON SYM="{phonemes}"/>'.format(phonemes=phonemes.replace('&', '&amp;'))
+def _generate_phoneme_xml(phonemes, phoneme_set='sapi'):
+    if phoneme_set == 'sapi':
+        valid_phonemes = {'&', '-', '!', '?', ',', '.', '_', 'aa', 'ae', 'ah', 'ao', 'aw', 'ax', 'ay', 'b', 'ch', 'd', 'dh', 'eh', 'er', 'ey', 'f', 'g', 'h', 'ih', 'iy', 'jh', 'k', 'l', 'm', 'n', 'ng', 'ow', 'oy', 'p', 'r', 's', 'sh', 't', 'th', 'uh', 'uw', 'v', 'w', 'y', 'z', 'zh'}
+        current_phonemes = set(map(lambda x:x.lower(), phonemes.split()))
+        invalid_phonemes = current_phonemes.difference(valid_phonemes)
+
+        if len(invalid_phonemes) > 0:
+            print(invalid_phonemes)
+            return ''
+
+    word_phonemes = phonemes.split('&')
+    ssml = ''
+    for word_phoneme in word_phonemes:
+        ssml += '<phoneme alphabet="sapi" ph="{phonemes}"></phoneme>'.format(phonemes=word_phoneme.strip().lower())
+
+    return '<speak version="1.0" xmlns="https://www.w3.org/2001/10/synthesis" xml:lang="en-US">{ssml}</speak>'.format(ssml=ssml)
+
 
 
 config = _load_config()
@@ -34,4 +49,5 @@ for row in reader:
     cols = len(row)
     sapi_input = _generate_phoneme_xml(row[2]) if cols > 2 else row[1]
     sapi_flags = flag_xml if cols > 2 else flag_not_xml
-    voice.create_recording('output/' + row[0] + '.wav', sapi_input, sapi_flags)
+    if sapi_input != '':
+        voice.create_recording('output/' + row[0] + '.wav', sapi_input, sapi_flags)
