dfdb8be751ccea317efbe89414e2d918298a6f7a
Jet Fontanilla <jetlogs@gmail.com>
2020-06-24 05:19:21 +0300

    use enums for constants

--- generate.py
+++ generate.py
@@ -3,7 +3,8 @@
 from os.path import dirname, realpath
 import csv
 import configparser
-from tts import sapi
+from tts.sapi import Sapi
+from tts.flags import SpeechVoiceSpeakFlags
 
 def _load_config():
     config = configparser.ConfigParser()
@@ -34,7 +35,7 @@ def _generate_phoneme_xml(phonemes, phoneme_set='sapi'):
 
 
 config = _load_config()
-voice = sapi.Sapi()
+voice = Sapi()
 
 voice.set_voice(config['voice'])
 voice.set_rate(int(config['rate']))
@@ -42,12 +43,9 @@ voice.set_volume(int(config['volume']))
 
 reader = csv.reader(_read_transcript_lines())
 
-flag_xml = 8
-flag_not_xml = 16
-
 for row in reader:
     cols = len(row)
     sapi_input = _generate_phoneme_xml(row[2]) if cols > 2 else row[1]
-    sapi_flags = flag_xml if cols > 2 else flag_not_xml
+    sapi_flags = SpeechVoiceSpeakFlags.IsXML.value if cols > 2 else SpeechVoiceSpeakFlags.Default.value
     if sapi_input != '':
         voice.create_recording('output/' + row[0] + '.wav', sapi_input, sapi_flags)
